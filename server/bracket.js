dc091845d4e4d3265e9d6eaf541fb429448a8a9e
bhavy996-creator <bhavymittal996@gmail.com>
2026-08-12 05:04:20 +0530

    redesign knockout bracket layout

--- server/bracket.js
+++ server/bracket.js
@@ -30,41 +30,54 @@ function renderRound(fixtures, title){
     const container = document.getElementById("nextRound");
     container.innerHTML = "";
 
-    fixtures.forEach((match) =>{
+    fixtures.forEach((match, index) =>{
         const card = document.createElement("div");
         card.className = "next-match";
         card.innerHTML = `
-        <div class="next-fixture">
-        <div class ="next-team home">
-        <img 
+
+<div class="bracket-header">
+
+    Quarter Final ${index + 1}
+
+</div>
+
+<div class="next-team home">
+
+    <img
         src="assets/flags/${match.home.toLowerCase()}.svg"
         class="flag"
         alt="${match.home}">
 
-        <span class ="next-team-name">${match.home}</span>
+    <span class="next-team-name">
 
-        </div>
+        ${match.home}
 
-        <div class="next-vs">
+    </span>
 
-            VS
+</div>
 
-        </div>
+<div class="vs-divider">
 
-        <div class="next-team away">
+    <span>VS</span>
 
-        <span class ="next-team-name">${match.away}</span>
+</div>
 
-            <img
-                src="assets/flags/${match.away.toLowerCase()}.svg"
-                class="flag"
-                alt="${match.away}">
+<div class="next-team away">
 
+    <img
+        src="assets/flags/${match.away.toLowerCase()}.svg"
+        class="flag"
+        alt="${match.away}">
 
-        </div>
+    <span class="next-team-name">
 
-    </div>
-        `;
+        ${match.away}
+
+    </span>
+
+</div>
+
+`;
         container.appendChild(card);
     });
 }
